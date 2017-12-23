13832b59f2053c3857b89343377a79198592f4c8
Rickard Johansson <rickard.joh@gmail.com>
2017-12-24 00:58:39 +0100

    Initial commit

---
+++ src/main/java/se/maokei/springfun/maokeiflux/MaokeinetfluxApplication.java
@@ -0,0 +1,12 @@
+package se.maokei.springfun.maokeiflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaokeinetfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaokeinetfluxApplication.class, args);
+	}
+}
---
+++ src/main/java/se/maokei/springfun/maokeiflux/domain/Document.java
@@ -0,0 +1,4 @@
+package se.maokei.springfun.maokeiflux.domain;
+
+public class Document {
+}
